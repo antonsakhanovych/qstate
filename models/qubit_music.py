3391241d01c47717d2e414eb2c2f5465a67a60e3
Anton Sakhanovych <antonsakhanovych@gmail.com>
2023-10-01 11:39:15 +0200

    example

--- models/qubit_music.py
+++ models/qubit_music.py
@@ -61,34 +61,34 @@ input_path = 'input.mid'
 create_melody(play_seq, input_path)
 ####################
 
-import pygame
+# import pygame
 
-def play_music(midi_filename):
-  '''Stream music_file in a blocking manner'''
-  clock = pygame.time.Clock()
-  pygame.mixer.music.load(midi_filename)
-  pygame.mixer.music.play()
-  while pygame.mixer.music.get_busy():
-    clock.tick(30) # check if playback has finished
+# def play_music(midi_filename):
+#   '''Stream music_file in a blocking manner'''
+#   clock = pygame.time.Clock()
+#   pygame.mixer.music.load(midi_filename)
+#   pygame.mixer.music.play()
+#   while pygame.mixer.music.get_busy():
+#     clock.tick(30) # check if playback has finished
 
 
-# mixer config
-freq = 44100  # audio CD quality
-bitsize = -16   # unsigned 16 bit
-channels = 2  # 1 is mono, 2 is stereo
-buffer = 1024   # number of samples
-pygame.mixer.init(freq, bitsize, channels, buffer)
+# # mixer config
+# freq = 44100  # audio CD quality
+# bitsize = -16   # unsigned 16 bit
+# channels = 2  # 1 is mono, 2 is stereo
+# buffer = 1024   # number of samples
+# pygame.mixer.init(freq, bitsize, channels, buffer)
 
-# optional volume 0 to 1.0
-pygame.mixer.music.set_volume(0.8)
+# # optional volume 0 to 1.0
+# pygame.mixer.music.set_volume(0.8)
 
-# listen for interruptions
-try:
-  # use the midi file you just saved
-  play_music(input_path)
-except KeyboardInterrupt:
-  # if user hits Ctrl/C then exit
-  # (works only in console mode)
-  pygame.mixer.music.fadeout(1000)
-  pygame.mixer.music.stop()
-  raise SystemExit
+# # listen for interruptions
+# try:
+#   # use the midi file you just saved
+#   play_music(input_path)
+# except KeyboardInterrupt:
+#   # if user hits Ctrl/C then exit
+#   # (works only in console mode)
+#   pygame.mixer.music.fadeout(1000)
+#   pygame.mixer.music.stop()
+#   raise SystemExit
